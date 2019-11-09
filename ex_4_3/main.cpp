4d4a47a58d9b5d8da3eda8874c660bfb65f290e4
v.medvedev <v.medvedev@conundrum.ai>
2019-11-09 12:38:52 +0300

    refactor for use templates and comparer

--- ex_4_3/main.cpp
+++ ex_4_3/main.cpp
@@ -8,33 +8,59 @@
  */
 #include <iostream>
 
-void swap(int *arr, int i, int j) {
-  int hold = arr[j];
-  arr[j] = arr[i];
-  arr[i] = hold;
-}
-
 // функция возвращает место опорного элмента в измененном массиве
-int partition(int *arr, int start, int stop, int pivot_index) {
-  swap(arr, stop - 1, pivot_index);
+template<typename T, typename CompareT>
+int partition(T *arr, int start, int stop, int pivot_index, CompareT compare) {
+  std::swap(arr[stop - 1], arr[pivot_index]);
   // i указывает на первый элемент больше опорного
   // j следующий не проверенный элемент
   int i = start;
-  while (arr[i] < arr[stop - 1]) {
+  while (compare(arr[i], arr[stop - 1])) {
     ++i;
   }
   int j = i + 1;
   while (j < stop - 1) {
-    if (arr[j] > arr[stop - 1]) {
+    if (compare(arr[stop - 1], arr[j])) {
       ++j;
     } else {
-      swap(arr, i, j);
+      std::swap(arr[i], arr[j]);
       ++i;
       ++j;
     }
   }
-  swap(arr, i, stop - 1);
+  std::swap(arr[i], arr[stop - 1]);
   return i;
+}
+
+template<typename T, typename CompareT>
+T get_k_order_statistic(int n, int k, T *arr, CompareT cmp) {
+  int start = 0;
+  int end = n;
+  int pivot_place = -1;
+  T pivot_element;
+  while (pivot_place != k) {
+    // рандомно выбираем опорный элемент
+    int pivot = start + (rand() % (end - start));
+    pivot_element = arr[pivot];
+    pivot_place = partition(arr, start, end, pivot, cmp);
+    if (k < pivot_place) {
+      end = pivot_place;
+    } else if (k > pivot_place) {
+      start = pivot_place + 1;
+    }
+  }
+  return pivot_element;
+}
+
+// Компаратор
+template<typename T>
+class ComparerLess {
+ public:
+  bool operator()(T &first, T &second) const;
+};
+template<typename T>
+bool ComparerLess<T>::operator()(T &first, T &second) const {
+  return first < second;
 }
 
 int main() {
@@ -46,21 +72,7 @@ int main() {
     std::cin >> arr[i];
   }
 
-  int start = 0;
-  int end = n;
-  int pivot_place = -1;
-  int pivot_element;
-  while (pivot_place != k) {
-    // рандомно выбираем опорный элемент
-    int pivot = start + (rand() % (end - start));
-    pivot_element = arr[pivot];
-    pivot_place = partition(arr, start, end, pivot);
-    if (k < pivot_place) {
-      end = pivot_place;
-    } else if (k > pivot_place) {
-      start = pivot_place + 1;
-    }
-  }
-  std::cout << pivot_element;
+  int k_order_statistic = get_k_order_statistic(n, k, arr, ComparerLess<int>());
+  std::cout << k_order_statistic;
   delete[] arr;
 }
