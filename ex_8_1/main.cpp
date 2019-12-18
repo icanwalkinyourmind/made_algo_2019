9632efdb1188afff10e81391ac258e0a36107fb2
v.medvedev <v.medvedev@conundrum.ai>
2019-12-18 15:38:26 +0300

    ex_8_1: fix issues

--- ex_8_1/main.cpp
+++ ex_8_1/main.cpp
@@ -10,11 +10,12 @@
 #include <iostream>
 #include <string>
 #include <functional>
+#include <assert.h>
 
 template<typename T, typename HashT>
 class OpenAddressingSet {
  public:
-  explicit OpenAddressingSet(HashT hash_function);
+  explicit OpenAddressingSet(HashT hash_function, T not_a_value);
   ~OpenAddressingSet();
   OpenAddressingSet(const OpenAddressingSet &) = delete;
   OpenAddressingSet(OpenAddressingSet &&) = delete;
@@ -25,25 +26,27 @@ class OpenAddressingSet {
   bool contains(T key) const;
  private:
   struct Node {
-    explicit Node(T key) {
-      this->key = key;
-    }
     T key;
-    bool available = true;
+    bool available = false;
   };
   const float resize_level = 0.75;
+  const float max_deleted_part = 0.75;
   int table_size = 8;
   int table_capacity = 0;
-  Node **table = new Node *[table_size]{};
+  int deleted_elements = 0;
+  Node *table = new Node[table_size]{};
   unsigned int get_key_position(T key) const;
+  void recreate_table(int new_size);
   void extend_table();
   HashT hash;
   unsigned int probe(int i, unsigned int hash_value) const;
+  T not_a_value;
 };
 
 template<typename T, typename HashT>
-OpenAddressingSet<T, HashT>::OpenAddressingSet(HashT hash_function) {
+OpenAddressingSet<T, HashT>::OpenAddressingSet(HashT hash_function, T not_a_value) {
   hash = hash_function;
+  this->not_a_value = not_a_value;
 }
 
 template<typename T, typename HashT>
@@ -61,7 +64,7 @@ unsigned int OpenAddressingSet<T, HashT>::get_key_position(T key) const {
   int i = 1;
   unsigned int hash_value = hash(key);
   unsigned int index = hash_value % table_size;
-  while (table[index] != nullptr && !(table[index]->key == key)) {
+  while (table[index].key != not_a_value && !(table[index].key == key)) {
     index = probe(i, hash_value);
     ++i;
   }
@@ -69,55 +72,56 @@ unsigned int OpenAddressingSet<T, HashT>::get_key_position(T key) const {
 }
 
 template<typename T, typename HashT>
-void OpenAddressingSet<T, HashT>::extend_table() {
+void OpenAddressingSet<T, HashT>::recreate_table(int new_size) {
   auto old_table = table;
   int old_table_size = table_size;
-  table_size = table_size * 2;
-  table = new Node *[table_size]{};
-
+  table = new Node[new_size]{};
   for (int i = 0; i < old_table_size; ++i) {
-    if (old_table[i] != nullptr and old_table[i]->available) {
-      table[get_key_position(old_table[i]->key)] = old_table[i];
+    if (old_table[i].key != not_a_value and old_table[i].available) {
+      table[get_key_position(old_table[i].key)] = old_table[i];
     }
   }
+  deleted_elements = 0;
   delete[] old_table;
 }
 
 template<typename T, typename HashT>
+void OpenAddressingSet<T, HashT>::extend_table() {
+  recreate_table(table_size * 2);
+}
+
+template<typename T, typename HashT>
 bool OpenAddressingSet<T, HashT>::put(T key) {
+  assert(key != not_a_value);
   if ((float) table_capacity / table_size > resize_level) extend_table();
   int key_position = get_key_position(key);
-  if (table[key_position] == nullptr) {
-    // ключа нет в таблице, просто вставляеем его
-    table[key_position] = new Node(key);
-    ++table_capacity;
-    return true;
-  } else if (!table[key_position]->available) {
-    // если ключь есть, но удалён, вставляем его на место первого встреченного удалённого элемента, или в пустую ячейку
-    // пердварительно удаляем старый элмент, чтобы не создавать дубликаты
-    delete table[key_position];
+  if (table[key_position].key != not_a_value and table[key_position].available) {
+    // ключ есть в таблце, операция не удалась
+    return false;
+  } else {
+    // если ключа нет или он удлаён, вставляем его на место первого встреченного удалённого элемента, или в пустую ячейку
     int i = 1;
     unsigned int hash_value = hash(key);
     unsigned int index = hash_value % table_size;
-    while (table[index] != nullptr && table[index]->available) {
+    while (table[index].key != not_a_value and table[index].available) {
       index = probe(i, hash_value);
       ++i;
     }
-    delete table[index];
-    table[index] = new Node(key);
+    table[index] = Node{key, true};
     ++table_capacity;
     return true;
-  } else {
-    return false;
   }
 }
 
 template<typename T, typename HashT>
 bool OpenAddressingSet<T, HashT>::remove(T key) {
   int key_position = get_key_position(key);
-  if (table[key_position] != nullptr and table[key_position]->available) {
-    table[key_position]->available = false;
+  if (table[key_position].key != not_a_value and table[key_position].available) {
+    table[key_position].available = false;
     --table_capacity;
+    ++deleted_elements;
+    // если количество удалённых элементов больше определнного порога, пересоздаём таблицу
+    if ((float) deleted_elements / table_size > max_deleted_part) recreate_table(table_size);
     return true;
   }
   return false;
@@ -126,7 +130,7 @@ bool OpenAddressingSet<T, HashT>::remove(T key) {
 template<typename T, typename HashT>
 bool OpenAddressingSet<T, HashT>::contains(T key) const {
   auto element = table[get_key_position(key)];
-  return element != nullptr and element->available;
+  return element.key != not_a_value and element.available;
 }
 
 void print_result(const std::string &s) {
@@ -143,7 +147,7 @@ int main() {
     return hash;
   };
 
-  auto set = new OpenAddressingSet<std::string, std::function<int(std::string)>>(hash_func);
+  auto set = new OpenAddressingSet<std::string, std::function<int(std::string)>>(hash_func, "");
   char operation;
   std::string s;
   while (std::cin >> operation >> s) {
@@ -158,6 +162,7 @@ int main() {
     operation_result ? print_result("OK") : print_result("FAIL");
   }
   delete set;
+  return 0;
 }
 
 
